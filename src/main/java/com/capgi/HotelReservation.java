8f089730ab36da8c5f149a092be3df2b9d4c8586
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-15 00:39:13 +0530

    [Shubham]Added: hotels to the reservation system

--- src/main/java/com/capgi/HotelReservation.java
+++ src/main/java/com/capgi/HotelReservation.java
@@ -3,7 +3,19 @@
  */
 package com.capgi;
 
+import java.util.ArrayList;
+
 public class HotelReservation {
+	ArrayList<Hotel> listOfHotels = new ArrayList<Hotel>();
+
+	public void addHotel(Hotel obj) {
+		listOfHotels.add(obj);
+	}
+
+	public int totalHotels() {
+		return listOfHotels.size();
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to hotel reservation system!");
 	}
